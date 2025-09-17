9a6cd223459b60694404ab363292b16bef3bf683
tarun goyal <tarungoyal@taruns-MacBook-Air.local>
2025-09-17 11:26:35 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import {Button} from "@mui/material";
+import CustomToolbar from "./widget/toolbar/Toolbar.tsx";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
   return (
-    <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+      <Button onClick={() => console.log("Increased")}>+ new</Button>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
+// import { Amplify } from "aws-amplify";
+// import outputs from "../amplify_outputs.json";
+import CssBaseline from "@mui/material/CssBaseline";
+import {createTheme, ThemeProvider} from "@mui/material";
 
-Amplify.configure(outputs);
+// Amplify.configure(outputs);
+
+const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
