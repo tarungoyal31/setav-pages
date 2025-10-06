7ca34d8be59b5e20346045dc77dad457225d6453
tarun goyal <tarungoyal@taruns-MacBook-Pro.local>
2025-10-06 10:23:47 +0530

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import {Button} from "@mui/material";
 import CustomToolbar from "./widget/toolbar/Toolbar.tsx";
 
 function App() {
-    return (
-        <main>
-            <CustomToolbar />
-            <h1>My todos</h1>
-            <Button onClick={() => console.log("Increased")}>+ new</Button>
-            <div>
-                🥳 App successfully hosted. Try creating a new todo.
-                <br/>
-                <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-                    Review next step of this tutorial.
-                </a>
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <CustomToolbar />
+      <h1>My todos</h1>
+      <Button onClick={() => console.log("Increased")}>+ new</Button>
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
+      </div>
+    </main>
+  );
 }
 
 export default App;
