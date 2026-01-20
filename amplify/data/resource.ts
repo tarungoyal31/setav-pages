5b288f98f8a6e2c6e29ef4fd227b9bc8e7d197ae
tarun goyal <tarungoyal@taruns-MacBook-Pro.local>
2026-01-20 08:56:02 +0530

    Remove TODO database

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+// import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -6,26 +6,26 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    // .authorization((allow) => [allow.publicApiKey()]),
-});
+// const schema = a.schema({
+//   Todo: a
+//     .model({
+//       content: a.string(),
+//     })
+//     // .authorization((allow) => [allow.publicApiKey()]),
+// });
+//
+// export type Schema = ClientSchema<typeof schema>;
 
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "iam",
-    // API Key is used for a.allow.public() rules
-    // apiKeyAuthorizationMode: {
-    //   expiresInDays: 30,
-    // },
-  },
-});
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     defaultAuthorizationMode: "iam",
+//     // API Key is used for a.allow.public() rules
+//     // apiKeyAuthorizationMode: {
+//     //   expiresInDays: 30,
+//     // },
+//   },
+// });
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
